8376bf56fcc2bdfd5a466bc760adbdaebd4d1b1b
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-03-08 18:49:54 +0100

    First commit

---
+++ src/StudyProject.Domain/Entities/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyProject.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+               
+        }
+    }
+}
---
+++ src/StudyProject.Infra.Data/Context/StudyProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyProject.Infra.Data.Context
+{
+    public class StudyProjectContext : DbContext
+    {
+        //public DbSet<Blog> Blogs { get; set; }
+        //public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+        }
+    }
+}
