f7fcc7fd2b70975669bc98ba2093ffc88fa8a3fb
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-03-09 12:25:39 +0100

    ModelCreating

--- src/StudyProject.Infra.Data/Context/StudyProjectContext.cs
+++ src/StudyProject.Infra.Data/Context/StudyProjectContext.cs
@@ -10,9 +10,40 @@ namespace StudyProject.Infra.Data.Context
         //public DbSet<Blog> Blogs { get; set; }
         //public DbSet<Post> Posts { get; set; }
 
+        public StudyProjectContext()
+        {
+
+        }
+
+        public StudyProjectContext(DbContextOptions<StudyProjectContext> options)
+            : base(options)
+        {
+
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Blogging;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<Blog>(entity =>
+            //{
+            //    entity.Property(e => e.Url).IsRequired();
+            //});
+
+            //modelBuilder.Entity<Post>(entity =>
+            //{
+            //    entity.HasOne(d => d.Blog)
+            //        .WithMany(p => p.Post)
+            //        .HasForeignKey(d => d.BlogId);
+            //});
         }
     }
 }
