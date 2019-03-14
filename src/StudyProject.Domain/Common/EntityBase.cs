f748e663fdef339092abb477edc5bad48a049699
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-03-14 16:17:35 +0100

    Add - Entity base

---
+++ src/StudyProject.Domain/Common/EntityBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace StudyProject.Domain.Common
+{
+ public abstract class EntityBase
+    {
+        public Guid Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as EntityBase;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(EntityBase a, EntityBase b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(EntityBase a, EntityBase b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [Id=" + Id + "]";
+        }
+    }
+}
