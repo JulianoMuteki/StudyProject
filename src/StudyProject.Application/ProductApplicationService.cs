56d097705cf2981910c204455f603dfd4e2af8e4
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-03-21 16:40:36 +0100

    Add - Product application service

---
+++ src/StudyProject.Application/ProductApplicationService.cs
@@ -0,0 +1,31 @@
+﻿using StudyProject.Domain.Entities;
+using StudyProject.Domain.Interfaces.Application;
+using StudyProject.Domain.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyProject.Application
+{
+    public class ProductApplicationService : IProductApplicationService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductApplicationService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IEnumerable<Product>> GetAllClient()
+        {
+            return await _unitOfWork.Repository<Product>().GetAllAsync();
+        }
+
+        public ICollection<Product> GetAll()
+        {
+            return _unitOfWork.Repository<Product>().GetAll();
+
+        }
+
+    }
+}
