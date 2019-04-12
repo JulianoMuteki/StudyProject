36a0af784478d4461ff89d80f01f76d1bcfb0bbd
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-04-12 21:14:05 +0200

    Add- Email Sender

--- src/StudyProject.UI.Web/Startup.cs
+++ src/StudyProject.UI.Web/Startup.cs
@@ -4,13 +4,18 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using StudyProject.Application.Services;
     using StudyProject.CrossCutting.Ioc.DependencyInjection;
     using StudyProject.Infra.Context;
+    using StudyProject.Infra.Identity.Models;
     using StudyProject.UI.WebCore.AutoMapper;
+    using System;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -31,7 +36,44 @@
             });
 
             services.AddDbContext<StudyProjectContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                                .AddEntityFrameworkStores<StudyProjectContext>()
+                                .AddDefaultTokenProviders();
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 6;
 
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Expiration = TimeSpan.FromDays(150);
+                // If the LoginPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/Login.
+                options.LoginPath = "/Account/Login";
+                // If the AccessDeniedPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/AccessDenied.
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddAutoMapperSetup();
@@ -45,6 +87,8 @@
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+              //  app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -56,6 +100,7 @@
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
