db105d90df2a0c64a52cfc89b2cc55b7b82fd130
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-11 15:50:54 +0200

    Add - Product validator

---
+++ src/StudyProject.Domain/Validations/ProductValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using StudyProject.Domain.Entities;
+
+namespace StudyProject.Domain.Validations
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name).NotNull().Length(0, 200);
+            RuleFor(x => x.Description).NotNull().Length(0, 250);
+        }
+    }
+}
